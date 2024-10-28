464bea4a181c7106ddaf515f2c6055fc4357afaa
Seongho <bluetosh7@gmail.com>
2024-10-28 18:04:14 +0900

    fix: use default texture wrap as CLAMP_TO_EDGE

--- cpps/src/Component/GrTextureComponent.cpp
+++ cpps/src/Component/GrTextureComponent.cpp
@@ -52,10 +52,10 @@ GrTextureComponent::GrTextureComponent(TextureType texture_type,
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   } else {
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   }
 
   // NOTE: If you need border, activate the below code (It's common for shadow
